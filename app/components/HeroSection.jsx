d6fedbcb21745e70b591acb3862e38eea1517e45
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-04-19 12:54:06 +0530

    version 1.1
    complete hero section

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,31 +1,63 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-            <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Charith</h1>
-            <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.</p>
-        </div>
-        <div>
-            <button>Hire Me</button>
-            <button>Download CV</button>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I'm{" "}
+            </span>
+            <br/>
+            <TypeAnimation
+              sequence={[
+                "Charith",
+                1000,
+                "Web Designer",
+                1000,
+                "Frontend Developer",
+                1000,
+                "Backend Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptuous.
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-            <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-                <Image src="/images/hero.png" alt="Hero Image" 
-                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                width={300} 
-                height={300} 
-                />
-            </div>
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero.png"
+              alt="Hero Image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
