0cdd11e0e2f18e1432e29943525d9f53ecba82e7
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-04-28 13:19:36 +0530

    version 1.8
    add overlay to project images

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React from "react";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 const ProjectCard = ({ imgUrl, title, description }) => {
-    return (
-      <div>
-        <div
-          className="h-52 md:h-72"
-          style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-        ></div>
-        <div className="text-white">
-          <h5>{title}</h5>
-          <p>{description}</p>
+  return (
+    <div>
+      <div
+        className="h-52 md:h-72 rounded-t-xl relative group"
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+      >
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+          <Link
+            href="/"
+            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-whit group/link"
+          >
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+          </Link>
+          <Link
+            href="/"
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-whit group/link"
+          >
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+          </Link>
         </div>
       </div>
-    );
-  };
-  
-  export default ProjectCard;
+      <div className="text-white rounded-t-xl bg-[#181818] py-6 px-4">
+        <h5 className="text-xl font-semibold md-2">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
