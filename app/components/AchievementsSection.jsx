5b56865a08f8a64d60da79da12b75003e590a268
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-05-07 11:43:52 +0530

    version 1.17
    add animation to Achievement section

--- app/components/AchievementsSection.jsx
+++ app/components/AchievementsSection.jsx
@@ -1,34 +1,59 @@
+"use client";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const AnimatedNumbers = dynamic(
+  () => {
+    return import("react-animated-numbers");
+  },
+  { ssr: false }
+);
 
 const achievementsList = [
   {
     metric: "Projects",
-    value: "100+",
+    value: "80",
+    postfix: "+",
   },
   {
+    prefix: "~",
     metric: "Users",
-    value: "1M+",
+    value: "100,000",
   },
   {
     metric: "Awards",
-    value: "10+",
+    value: "7",
   },
   {
     metric: "Years",
-    value: "5+",
+    value: "5",
   },
 ];
 const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="border-[#33353F] border rounded-md py-8 px-17 flex flex-row items-center justify-between">
+      <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
         {achievementsList.map((achievement, index) => (
           <div
             key={index}
             className="flex flex-col items-center justify-center mx-4"
           >
-            <h2 className="text-white text-4xl font-bold">
-              {achievement.value}
+            <h2 className="text-white text-4xl font-bold flex flex-row">
+              {achievement.prefix}
+              <AnimatedNumbers
+                includeComma
+                animateToNumber={parseInt(achievement.value)}
+                locale="en-US"
+                className="text-white text-4xl font-bold"
+                configs={(_, index) => {
+                  return {
+                    mass: 1,
+                    friction: 100,
+                    tensions: 140 * (index + 1),
+                  }
+                }}
+                />
+              {achievement.postfix}
             </h2>
             <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
           </div>
--- app/components/TabButton.jsx
+++ app/components/TabButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TabButton = ({ active, selectTab, children }) => {
   const buttonClasses = active
-    ? "text-white border-b border-purple-500"
+    ? "text-white border-b border-primary-500"
     : "text-[#ADB7BE]";
 
   return (
