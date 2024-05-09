b4ae83c00819c906bcefb3b9d2e37299d8449249
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-05-09 22:45:03 +0530

    version 1.20
    fix some bugs when deploy it.

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -37,11 +37,11 @@ const EmailSection = () => {
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I'm always open to new projects, collaborations, or employment
-          opportunities. Let's connect and build something great together.
+          I&apos;m always open to new projects, collaborations, or employment
+          opportunities. Let&apos;s connect and build something great together.
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="github.com">
