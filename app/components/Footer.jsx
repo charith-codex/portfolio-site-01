93dd63b03dbdd5073acdc9a1950cbdabcadcde56
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-05-19 15:54:21 +0530

    version 1.21.1
    remove ellipse

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span className="text-slate-500">2024</span>
+        <span className="text-slate-500">@2024</span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
